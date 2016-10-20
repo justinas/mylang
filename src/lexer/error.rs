5a1054873d4d1e6fdcdaf7f22cd7042be584f325
Justinas Stankevicius <justinas.stankevicius@adform.com>
2016-10-20 18:33:11 +0300

    CLI

--- src/lexer/error.rs
+++ src/lexer/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Eq, PartialEq)]
 pub enum ErrorVariant {
     EOF,
@@ -5,9 +7,30 @@ pub enum ErrorVariant {
     InvalidStringEscape,
 }
 
+impl fmt::Display for ErrorVariant {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let desc = match *self {
+            ErrorVariant::EOF => "unexpected end of file",
+            ErrorVariant::UnknownCharacter => "unknown character occured",
+            ErrorVariant::InvalidStringEscape => "invalid escape character in string",
+        };
+        write!(f, "{}", desc)
+    }
+}
+
 #[derive(Debug)]
 pub struct Error {
     pub line: u64,
     pub chr: u64,
     pub var: ErrorVariant,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,
+               "Lexer error: {} at line {}, char {}",
+               self.var,
+               self.line,
+               self.chr)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,47 @@
-mod lexer;
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{stderr, Write};
+
+fn pretty_print(tokens: &Vec<lexer::Token>) {
+    println!("Tokens:");
+    for t in tokens {
+        println!("    {:?}", t);
+    }
 }
+
+fn main() {
+    let filename = match std::env::args().skip(1).next() {
+        Some(f) => f,
+        None => {
+            write!(stderr(), "Usage: ./mylang [filename]");
+            return;
+        }
+    };
+
+    let f = match File::open(&filename) {
+        Ok(f) => f,
+        Err(e) => {
+            write!(stderr(), "Error while opening {}: {}", filename, e);
+            return;
+        }
+    };
+
+    let lexer = match lexer::Lexer::new(f) {
+        Ok(l) => l,
+        Err(e) => {
+            write!(stderr(), "Lexer I/O error: {:?}", e);
+            return;
+        }
+    };
+
+    match lexer.lex() {
+        Ok(tokens) => {
+            pretty_print(&tokens);
+        }
+        Err((tokens, e)) => {
+            pretty_print(&tokens);
+            write!(stderr(), "{}", e);
+        }
+    }
+}
+
+mod lexer;
